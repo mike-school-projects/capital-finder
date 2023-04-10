9fd2fcc16b71cc647801716c7081916b1c57ca20
Mike Shen <michael.m.shen@gmail.com>
2023-04-10 22:11:22 +0000

    added files

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from urllib import parse
-url_components = parse.urlsplit("https://capital-finder-mike.vercel.app/api/capital-finder?esaf=Santiago")
+url_components = parse.urlsplit("https://capital-finder-mike.vercel.app/api/capital-finder?country=Santiago")
 query_string_list = parse.parse_qsl(url_components.query)
 dic = dict(query_string_list)
 print(dic)
 
 if "country" in dic:
-    print("country")
+    url = "https://restcountries.com/v3.1/name/" + dic["country"]
+    print(url)
 
 elif "capital" in dic:
     print("capital")

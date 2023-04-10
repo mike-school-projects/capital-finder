63da48728c4020d08cfb8d1be068acc73a4e4623
Mike Shen <michael.m.shen@gmail.com>
2023-04-10 21:43:14 +0000

    added files

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -13,34 +13,19 @@ class handler(BaseHTTPRequestHandler):
 
         # Pulls out query
         query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
 
-        if "country" in dic:
-            # url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+        # dic = dict(query_string_list)
 
-            # Base URL
-            url = "https://restcountries.com/v3.1/name/"
+        # Base URL
+        url = "https://restcountries.com/v3.1/name/"
 
-            # Full URL with endpoint
-            r = requests.get(url + dic["word"])
+        # Get data from API using base URL plus country
+        r = requests.get(url + query_string_list)
 
-            # Get data from API as json
-            data = r.json()
+        # Get data from API as json
+        data = r.json()
 
-            with open("test.json", "w") as file:
-                file.write(data)
-
-            #
-            capital = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                capital.append(definition)
-            message = str(capital)
-
-        else:
-            message = "Give me a word to define please"
-
-        message = str(query_string_list)
+        message = str(data)
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
